89d28b1433888f6cfe05606fcf0b25f094936105
Yauhen Lis <jfox1389@yandex.ru>
2023-05-25 20:36:04 +0300

    re_path и path

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -14,3 +14,12 @@ def about(request):
 
 def contact(request):
     return HttpResponse("<h2>Контакты</h2>")
+
+def products(request,productid=1):
+
+    output = f"<h2>Продукт № {productid}</h2>"
+    return HttpResponse(output)
+
+def users(request,id=1,name = "Neo"):
+    output = f"<h2>Пользователь</h2><h3>id: {id} Имя: {name}</h3>"
+    return HttpResponse(output)
