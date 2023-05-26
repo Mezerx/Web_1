f802ded6aa3da8a43d71134fba17b7580ca97ff1
Yauhen Lis <jfox1389@yandex.ru>
2023-05-26 19:58:14 +0300

    переадресация

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,25 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect , HttpResponsePermanentRedirect
 
 
 # Create your views here.
 
-
 def index(request):
-    return HttpResponse("<h2>Главная</h2>")
+    return HttpResponse("Index")
 
 def about(request):
-    return HttpResponse("<h2>О сайте</h2>")
-
+    return HttpResponse("About")
 
 def contact(request):
-    return HttpResponse("<h2>Контакты</h2>")
+    return HttpResponseRedirect("/about")
 
-def products(request,productid=1):
-
-    output = f"<h2>Продукт № {productid}</h2>"
-    return HttpResponse(output)
-
-def users(request,id=1,name = "Neo"):
-    output = f"<h2>Пользователь</h2><h3>id: {id} Имя: {name}</h3>"
-    return HttpResponse(output)
+def details(reqest):
+    return HttpResponseRedirect('/')
