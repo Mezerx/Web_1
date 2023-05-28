91681eef7c26c7a2b500b8a1cce7dd87fce64b35
Yauhen Lis <jfox1389@yandex.ru>
2023-05-28 21:50:24 +0300

    основы css

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -5,12 +5,8 @@ from django.http import HttpResponse, HttpResponseRedirect , HttpResponsePermane
 # Create your views here.
 
 def index(request):
-    header = "Персональные данные"
-    langs = ["Английский","Немецкий","Испанский"]
-    user = {"name" : "Максим" , "age" : 30}
-    addr = ["Виноградная", 23 , 45]
-    data = {"header": header , "langs" : langs, "user" : user, "addr" : addr}
-    return render(request, "index.html", context=data)
+
+    return render(request, "firstapp/home.html")
 
 def about(request):
     return HttpResponse("About")
