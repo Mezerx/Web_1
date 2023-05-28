a620342d6028f73607376b284140f76cb8f8cf8a
Yauhen Lis <jfox1389@yandex.ru>
2023-05-28 21:23:52 +0300

    передача данных в шаблон

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -5,7 +5,12 @@ from django.http import HttpResponse, HttpResponseRedirect , HttpResponsePermane
 # Create your views here.
 
 def index(request):
-    return render(request, "firstapp/home.html")
+    header = "Персональные данные"
+    langs = ["Английский","Немецкий","Испанский"]
+    user = {"name" : "Максим" , "age" : 30}
+    addr = ["Виноградная", 23 , 45]
+    data = {"header": header , "langs" : langs, "user" : user, "addr" : addr}
+    return render(request, "index.html", context=data)
 
 def about(request):
     return HttpResponse("About")
