0a0012d7feb97b922bf48ed658f4f569e169f14b
Yauhen Lis <jfox1389@yandex.ru>
2023-05-30 20:14:14 +0300

    использование класса TemplateView

--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from django.urls import re_path
+from django.views.generic import TemplateView
 from firstapp import views
 
 
 urlpatterns = [
     path('',views.index),
-    path('about/', views.about),
+    path('about/', TemplateView.as_view(template_name="firstapp/about.html")),
+    path('contact/',TemplateView.as_view(template_name="firstapp/contact.html",extra_context= {"work":"Разработка программных продуктов"})),
+
 
 ]
