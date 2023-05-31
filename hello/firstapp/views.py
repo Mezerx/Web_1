7bf8276952ad0c21700abaf5b1c0ccaa023332e1
Yauhen Lis <jfox1389@yandex.ru>
2023-05-31 20:28:12 +0300

    тег для ввода информации по условию

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect , HttpResponsePermanentRedirect
+from django.http import *
 
 
 # Create your views here.
 
 def index(request):
-
-    return render(request, "firstapp/index.html")
+    data = {"age" : 66}
+    return render(request, "firstapp/index.html", context=data)
 
 def about(request):
     return HttpResponse("About")
