8ecaba22783a5478c472ea6a382e6b25b910399f
Yauhen Lis <jfox1389@yandex.ru>
2023-05-31 20:37:35 +0300

    тег для цикла

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -5,8 +5,8 @@ from django.http import *
 # Create your views here.
 
 def index(request):
-    data = {"age" : 66}
-    return render(request, "firstapp/index.html", context=data)
+    cat = []
+    return render(request, "firstapp/index.html", context={"cat": cat})
 
 def about(request):
     return HttpResponse("About")
