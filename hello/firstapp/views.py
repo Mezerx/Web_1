3681ded135e2055edde6148e4daa3643de16ddec
Yauhen Lis <jfox1389@yandex.ru>
2023-05-31 20:45:35 +0300

    тег для для задания значений переменным

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -5,7 +5,7 @@ from django.http import *
 # Create your views here.
 
 def index(request):
-    cat = []
+    cat = ["Ноутбуки", "Принтеры", "Сканеры", "Диски", "Шнуры"]
     return render(request, "firstapp/index.html", context={"cat": cat})
 
 def about(request):
