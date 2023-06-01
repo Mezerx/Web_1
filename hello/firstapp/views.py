23bd1eaee9fe7933b404b5c08d41090173e6e4ff
Yauhen Lis <jfox1389@yandex.ru>
2023-06-01 21:16:03 +0300

    POST - запросы

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .forms import UserForm
 
 
 # Create your views here.
 
 def index(request):
-    userform = UserForm()
-    return render(request, "firstapp/index.html", {"form":userform})
+    if request.method == "POST":
+        name = request.POST.get('name') # получить значение поля Имя
+        age = request.POST.get('age') # получить значение поля Возраст
+        output = f"<h2>Пользователь</h2><h3>Имя - {name}, " \
+                 f"Возраст - {age}</h3>"
+        return HttpResponse(output)
+    else:
+        userform = UserForm()
+        return render(request, 'firstapp/index.html', {'form': userform})
+
+
+
+
+
 
 def about(request):
     return HttpResponse("About")
