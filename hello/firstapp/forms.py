175541387dee2c4f97661363b93ade1d4a10fa73
Yauhen Lis <jfox1389@yandex.ru>
2023-06-03 20:43:18 +0300

    EmailField ввод электронного адреса

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -5,4 +5,4 @@ from django import forms
 
 
 class UserForm(forms.Form):
-    name = forms.CharField(label="Имя клиента", max_length=15, help_text="ФИО не более 15 символов",required=False)
+    email = forms.EmailField(label="Электронный адрес", help_text="Обязательный символ - @ ")
