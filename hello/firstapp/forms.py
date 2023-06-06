31cff19214ec03d0e2952b61584f1eb136a9eb95
Yauhen Lis <jfox1389@yandex.ru>
2023-06-06 21:03:58 +0300

    детальная настройка полей формы

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -5,6 +5,6 @@ from django import forms
 
 
 class UserForm(forms.Form):
-   name = forms.CharField(label="Введите имя")
-   age = forms.IntegerField(label="Введите возраст")
+   name = forms.CharField(label="Имя клиента")
+   age = forms.IntegerField(label="Возраст клиента")
 
--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -7,6 +7,11 @@ from .forms import UserForm
 
 def index(request):
     userform = UserForm()
+    if request.method == "POST":
+        userform = UserForm(request.POST)
+        if userform.is_valid():
+            name = userform.cleaned_data["name"]
+            return HttpResponse(f"<h2>Имя введено корректно - {name}</h2>")
     return render(request, 'firstapp/index.html', {'form': userform})
 
 
