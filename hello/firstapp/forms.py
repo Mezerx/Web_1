72279116e46a73da6fc8874874c8874b39ea2da0
Yauhen Lis <jfox1389@yandex.ru>
2023-06-06 15:43:32 +0300

    help_tetxt  - задание подсказки

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -5,6 +5,6 @@ from django import forms
 
 
 class UserForm(forms.Form):
-   name = forms.CharField(label="Введите имя",initial="username")
-   age = forms.IntegerField(label="Введите возраст",initial="18")
+   name = forms.CharField(label="Введите имя",help_text="Введите ФИО")
+   age = forms.IntegerField(label="Введите возраст",help_text="Введите возраст")
 
--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -6,7 +6,7 @@ from .forms import UserForm
 # Create your views here.
 
 def index(request):
-    userform = UserForm(field_order=["age","name"])
+    userform = UserForm()
     return render(request, 'firstapp/index.html', {'form': userform})
 
 
