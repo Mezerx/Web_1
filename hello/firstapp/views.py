e732018c1effe7d076105dd47b206aa1164320ac
Yauhen Lis <jfox1389@yandex.ru>
2023-06-06 15:38:32 +0300

    задание порядок следования полей на форме

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -6,15 +6,8 @@ from .forms import UserForm
 # Create your views here.
 
 def index(request):
-    if request.method == "POST":
-        name = request.POST.get('name') # получить значение поля Имя
-        age = request.POST.get('age') # получить значение поля Возраст
-        output = f"<h2>Пользователь</h2><h3>Имя - {name}, " \
-                 f"Возраст - {age}</h3>"
-        return HttpResponse(output)
-    else:
-        userform = UserForm()
-        return render(request, 'firstapp/index.html', {'form': userform})
+    userform = UserForm(field_order=["age","name"])
+    return render(request, 'firstapp/index.html', {'form': userform})
 
 
 
