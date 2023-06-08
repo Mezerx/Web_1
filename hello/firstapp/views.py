39246ccfec4a90a2257d3eb6102b5c8db7638b30
Yauhen Lis <jfox1389@yandex.ru>
2023-06-08 21:00:13 +0300

    чтение и запись данных в БД

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .forms import UserForm
+from django.http import HttpResponseRedirect
+from .models import Person
 
 
 # Create your views here.
-
+#получение данных из бд и загрузка index.html
 def index(request):
-    userform = UserForm()
-    if request.method == "POST":
-        userform = UserForm(request.POST)
-        if userform.is_valid():
-            name = userform.cleaned_data["name"]
-            return HttpResponse(f"<h2>Имя введено корректно - {name}</h2>")
-    return render(request, 'firstapp/index.html', {'form': userform})
+    people = Person.objects.all()
+    return render(request, 'index.html', {'people': people})
 
+#сохранение данных в бд
+def create(request):
+    if request.method == 'POST':
+        klient = Person()
+        klient.name = request.POST.get('name')
+        klient.age = request.POST.get('age')
+        klient.save()
+    return HttpResponseRedirect('/')
 
 
 
--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -6,8 +6,7 @@ from firstapp import views
 
 urlpatterns = [
     path('',views.index),
-    path('about/', TemplateView.as_view(template_name="firstapp/about.html")),
-    path('contact/',TemplateView.as_view(template_name="firstapp/contact.html",extra_context= {"work":"Разработка программных продуктов"})),
+    path("create/", views.create),
 
 
 ]
