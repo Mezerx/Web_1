56e51ffcb0656f37c21213392314fbd5466d56ee
Yauhen Lis <jfox1389@yandex.ru>
2023-06-11 20:53:45 +0300

    редактирование/удаление данных

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -18,6 +18,28 @@ def create(request):
         klient.save()
     return HttpResponseRedirect('/')
 
+#изменение данных в бд
+def edit(request,id):
+    try:
+        person = Person.objects.get(id=id)
+        if request.method == 'POST':
+            person.name = request.POST.get('name')
+            person.age = request.POST.get('age')
+            person.save()
+            return HttpResponseRedirect('/')
+        else:
+            return render(request, 'edit.html', {'person': person})
+    except Person.DoesNotExist:
+        return HttpResponseNotFound("<h2>Клиент не найден</h2>")
+
+#удаление данных из бд
+def delete(request,id):
+    try:
+        person = Person.objects.get(id=id)
+        person.delete()
+        return HttpResponseRedirect('/')
+    except Person.DoesNotExist:
+        return HttpResponseNotFound("<h2>Клиент не найден</h2>")
 
 
 
--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -7,6 +7,9 @@ from firstapp import views
 urlpatterns = [
     path('',views.index),
     path("create/", views.create),
+    path('edit/<int:id>', views.edit),
+    path('delete/<int:id>', views.delete),
+
 
 
 ]
