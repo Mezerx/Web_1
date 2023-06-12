95de4e0c759d7477e8fe44e42ad9c562f886139c
Yauhen Lis <jfox1389@yandex.ru>
2023-06-12 20:45:07 +0300

    организация связей "один-ко-многим"

--- hello/firstapp/models.py
+++ hello/firstapp/models.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 from django.db import models
 
 class Person(models.Model):
@@ -5,5 +7,37 @@ class Person(models.Model):
     age = models.IntegerField()
     objects = models.Manager()
     DoesNotExist = models.Manager
+
+class Company(models.Model):
+    name = models.CharField(max_length=30)
+
+class Product(models.Model):
+    company = models.ForeignKey(Company,on_delete=models.CASCADE)
+    name=models.CharField(max_length=30)
+    price=models.IntegerField()
+
+
+#Создаем объект Company с именем Электрон
+firma = Company.objects.create(name="Электрон")
+
+#создание товара компании
+firma.product_set.crete(name="Samsung",price="4200")
+
+#отдельное создание объекта с последующим добавлением в бд
+ipad = Product(name="ipad", price="3200")
+#при добавлении необходимо указать параметр bulk=False
+firma.product_set.add(ipad,bulk=False)
+
+
+#исключает из компании все товары
+#при этом товары остаются в бд и не привязаны к компании
+#работает, если в зависимой модели ForeignKey(Company, null = True)
+#firma.product_set.clear()
+
+
+#тоже самое, только в отношении одного объекта
+#ipad = Product.objects.get(name="ipad")
+#firma.product_set.remove(ipad)
+
 
 
